4f4431783032525438308dac5700910d6d6e6705
unknown <anand.krishna5154@gmail.com>
2024-08-12 13:34:17 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'Kingdom of the Planet of Apes',
+        des: 'Many generations after Caesars death, apes have established numerous clans, while humans have become animalistic and unable to speak. Noa, a young chimpanzee from a falconry-practicing clan,prepares for a coming-of-age ceremony by collecting wild eagle eggs with his friends Anaya andSoona',
+        image: 'images/slider1.png'
+    },
+    {
+        name: 'Varathan',
+        des: 'After losing his job, Aby and his wife, Priya, move back to the latters family estate in Kerala. However, things go from bad to worse for the couple as they encounter a new set of challenges.',
+        image: 'images/slider2.png'
+    },
+    {
+        name: 'Sita Ramam',
+        des: 'An orphaned soldier life changes after he receives a letter from a girl named Sita. He meets her and love blossoms between them. When he returns to her camp in Kashmir, he sends a letter to Sita that will not reach her.',
+        image: 'images/slider3.png'
+    },
+    {
+        name: 'Chutney Sambar',
+        des: 'The series tells the story of an orphaned street vendor Sachin (Yogi Babu) who unexpectedly becomes part of his wealthy half-brother family, navigating the challenges of gaining acceptance in his new circumstances.',
+        image: 'images/slider4.png'
+    },
+    {
+        name: 'Guruvayoor Ambalanadayil',
+        des: 'A young man faces a web of unforeseen bad luck before marrying a woman who hates him, and he suffers her wrath over it.',
+        image: 'images/slider5.png'
+    }
+]
